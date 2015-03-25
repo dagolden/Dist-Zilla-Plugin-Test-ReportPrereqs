2013a526b202af9f1a440adca99d4757d29d9e19
David Golden <dagolden@cpan.org>
2015-03-24 23:39:15 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/Test/ReportPrereqs.pm
+++ lib/Dist/Zilla/Plugin/Test/ReportPrereqs.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::Test::ReportPrereqs;
 # ABSTRACT: Report on prerequisite versions during automated testing
 
-our $VERSION = '0.021';
+our $VERSION = '0.022';
 
 use Dist::Zilla 4 ();
 
